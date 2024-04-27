5f6785e751a9e90218b7232fb47b077b23d25e51
makirin <maki.rincha.aries@gmail.com>
2024-04-27 15:46:27 +0900

    店舗管理にカテゴリ・定休日を追加

---
+++ app/Models/RegularHoliday.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\DB;
+
+class RegularHoliday extends Model
+{
+    use HasFactory;
+
+    public function restaurants(): BelongsToMany
+    {
+        return $this->belongsToMany(Restaurants::class);
+    }
+}
