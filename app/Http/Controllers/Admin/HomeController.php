57ad0d94f0af2b8cb3ece6925ac658a36028df62
makirin <maki.rincha.aries@gmail.com>
2024-05-11 09:47:33 +0900

    管理者側トップページ

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,13 +1,31 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('admin.home');
+        $total_users = User::get()->count();
+        Log::debug("----------------");
+        Log::debug($total_users);
+        $total_premium_users = DB::table('subscriptions')->select('stripe_status')->get()->count();
+        Log::debug($total_premium_users);
+        $total_free_users = $total_users - $total_premium_users;
+        Log::debug($total_free_users);
+        $total_restaurants = Restaurant::get()->count();
+        Log::debug($total_restaurants);
+        $total_reservations = Reservation::get()->count();
+        Log::debug($total_reservations);
+        $sales_for_this_month = $total_premium_users * 300;
+        Log::debug($sales_for_this_month);
+        return view('admin.home', compact('total_users', 'total_premium_users', 'total_free_users', 'total_restaurants', 'total_reservations', 'sales_for_this_month'));
     }
 }
